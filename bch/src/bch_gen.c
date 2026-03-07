2d682b233564b963023578d622e02ee7521e189d
Josh Chan <jfusion3600@gmail.com>
2026-03-07 15:07:57 +1100

    Generate the generator polynomial based on parameters

--- bch/src/bch_gen.c
+++ bch/src/bch_gen.c
@@ -4,6 +4,11 @@
 
 int bch_init(bch_ctx_t *bch, int m, uint32_t prim_poly, int t) {
     memset(bch, 0, sizeof(*bch));
+
+    if (m <= 1 || m >= 16 || t <= 0) {
+        return -1;
+    }
+
     bch->m = m;
     bch->t = t;
     bch->n = (1 << m) - 1;
@@ -11,18 +16,85 @@ int bch_init(bch_ctx_t *bch, int m, uint32_t prim_poly, int t) {
     bch->gf.m = m;
     bch->gf.prim_poly = prim_poly;
 
-    // TODO: build generator polynomial g(x) for BCH(t)
-    // For now: placeholder g(x)=1 (deg 0) so k=n (not a real BCH yet)
-    bch->dg = 8;
-    bch->k = bch->n - bch->dg;
-    bch->g = (uint8_t*)calloc((size_t)(bch->dg + 1), 1);
-    if (!bch->g) return -1;
+    // Sanity check: if 2t >= n, we can't have a valid code.
+    if (2 * t >= bch->n) {
+        return -1;
+    }
 
-    bch->g[0] = 1;
-    bch->g[4] = 1;
-    bch->g[6] = 1;
-    bch->g[7] = 1;
-    bch->g[8] = 1;
+    uint16_t *alpha_pow = (uint16_t *)calloc((size_t)bch->n, sizeof(uint16_t));
+    uint8_t *in_union = (uint8_t *)calloc((size_t)bch->n, 1);
+    uint16_t *poly = (uint16_t *)calloc((size_t)(bch->n + 1), sizeof(uint16_t));
+    uint16_t *tmp = (uint16_t *)calloc((size_t)(bch->n + 1), sizeof(uint16_t));
+    if (!alpha_pow || !in_union || !poly || !tmp) {
+        free(alpha_pow);
+        free(in_union);
+        free(poly);
+        free(tmp);
+        return -1;
+    }
+
+    alpha_pow[0] = 1u;
+    for (int e = 1; e < bch->n; e++) {
+        alpha_pow[e] = gf_mul(&bch->gf, alpha_pow[e - 1], 0x2u);
+    }
+
+    for (int i = 1; i <= 2 * t; i++) {
+        int e = i % bch->n;
+        while (!in_union[e]) {
+            in_union[e] = 1;
+            e = (2 * e) % bch->n;
+        }
+    }
+
+    int dg = 0;
+    poly[0] = 1u;
+
+    for (int e = 1; e < bch->n; e++) {
+        if (!in_union[e]) {
+            continue;
+        }
+
+        const uint16_t root = alpha_pow[e];
+        memset(tmp, 0, (size_t)(bch->n + 1) * sizeof(uint16_t));
+
+        for (int k = 0; k <= dg; k++) {
+            tmp[k] ^= gf_mul(&bch->gf, poly[k], root);
+            tmp[k + 1] ^= poly[k];
+        }
+
+        memcpy(poly, tmp, (size_t)(bch->n + 1) * sizeof(uint16_t));
+        dg++;
+    }
+
+    bch->g = (uint8_t *)calloc((size_t)(dg + 1), 1);
+    if (!bch->g) {
+        free(alpha_pow);
+        free(in_union);
+        free(poly);
+        free(tmp);
+        return -1;
+    }
+
+    for (int j = 0; j <= dg; j++) {
+        if (!(poly[j] == 0u || poly[j] == 1u)) {
+            free(alpha_pow);
+            free(in_union);
+            free(poly);
+            free(tmp);
+            bch_free(bch);
+            return -1;
+        }
+        bch->g[j] = (uint8_t)poly[j];
+    }
+
+    bch->dg = dg;
+    bch->k = bch->n - dg;
+
+    free(alpha_pow);
+    free(in_union);
+    free(poly);
+    free(tmp);
+
     return 0;
 }
 
